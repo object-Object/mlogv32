c0f7d8036df306f6accea129a86b25f557af8022
object-Object <object@objectobject.ca>
2025-05-13 16:49:28 -0400

    Implement SLL(I), SRL(I), and SRA(I)

--- asm/math.s
+++ asm/math.s
@@ -1,15 +1,35 @@
 .global _start
 _start:
-    li x1, 0b0101
-    li x2, 0b0011
-    xor x3, x1, x2 # should be 0b0110 == 6
+    li x1, 0b1
+    li x2, 1
+    sll x3, x1, x2
     
-    li x4, 0b0101
-    li x5, 0b0011
-    or x6, x4, x5 # should be 0b0111 == 7
+    li x4, 0xffffffff
+    li x5, 31
+    sll x6, x4, x5
 
-    li x7, 0b0101
-    li x8, 0b0011
-    and x9, x7, x8 # should be 0b0001 == 1
+    li x7, 0b10000000000000000000000000000000
+    li x8, 1
+    srl x9, x7, x8
+
+    li x10, 0b10000000000000000000000000000000
+    li x11, 1
+    sra x12, x10, x11
+
+    li x13, 1
+    li x14, 1
+    srl x15, x13, x14
+
+    li x16, 1
+    li x17, 1
+    sra x18, x16, x17
+
+    li x19, 0
+    li x20, 1
+    srl x21, x19, x20
+
+    li x22, 0
+    li x23, 1
+    sra x24, x22, x23
 
     j 0

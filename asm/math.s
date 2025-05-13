42ba5a621dff864f9e86388d1730278b53b05d73
object-Object <object@objectobject.ca>
2025-05-13 15:42:03 -0400

    Implement XOR(I), OR(I), AND(I)

--- asm/math.s
+++ asm/math.s
@@ -1,23 +1,15 @@
 .global _start
 _start:
-    li x1, 1
-    li x2, 1
-    slt x3, x1, x2
+    li x1, 0b0101
+    li x2, 0b0011
+    xor x3, x1, x2 # should be 0b0110 == 6
     
-    li x4, 1
-    li x5, 2
-    slt x6, x4, x5
+    li x4, 0b0101
+    li x5, 0b0011
+    or x6, x4, x5 # should be 0b0111 == 7
 
-    li x7, -1
-    li x8, 1
-    slt x9, x7, x8
-
-    li x10, 10
-    li x11, -2
-    sub x12, x10, x11
-
-    li x13, 10
-    li x14, 12
-    sub x15, x13, x14
+    li x7, 0b0101
+    li x8, 0b0011
+    and x9, x7, x8 # should be 0b0001 == 1
 
     j 0

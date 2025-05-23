b19622516040d8ad94d80bc542bf754b833349a8
object-Object <object@objectobject.ca>
2025-05-23 16:23:37 -0400

    Move rodata into ROM and add missing .data initializer

--- coremark/mlogv32/entry.s
+++ coremark/mlogv32/entry.s
@@ -7,6 +7,20 @@ _start:
     add s0, sp, zero
 
     la gp, __global_pointer$
+
+    # Initialize .data
+    # https://sourceware.org/binutils/docs/ld/Output-Section-LMA.html#Output-Section-LMA
+    la t0, __sidata
+    la t1, __sdata
+    la t2, __edata
+load_data:
+    bgeu t1, t2, data_done
+    lb t3, 0(t0)
+    sb t3, 0(t1)
+    addi t0, t0, 1
+    addi t1, t1, 1
+    j load_data
+data_done:
     
     # Clear BSS section
     la t0, __sbss

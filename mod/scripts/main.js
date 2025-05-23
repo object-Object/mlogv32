137ddd1682a8776dae46042189c7a906bac44eee
object-Object <object@objectobject.ca>
2025-05-23 16:13:33 -0400

    Rewrite mlogv32-utils to show progress and allow dumping memory

--- mod/scripts/main.js
+++ mod/scripts/main.js
@@ -9,106 +9,262 @@ global.override.block(LogicBlock, {
      * @param {Table} table
      */
     buildConfiguration(table) {
+        /** @type Table */
         const buttons = table.table().get();
         this.super$buildConfiguration(buttons);
 
-        if (isMlogv32Processor(this)) {
+        const memory = Mlogv32Memory_of(this);
+        if (memory != null) {
             buttons
                 .button(Icon.download, Styles.cleari, () => {
                     Vars.platform.showFileChooser(true, "bin", (file) => {
-                        try {
-                            flashMlogv32Processor(this, file);
-                        } catch (e) {
-                            Log.err(e);
-                            Vars.ui.hudfrag.showToast(
-                                Icon.warning,
-                                "Failed to flash processor RAM: " + e
-                            );
-                        }
+                        memory("flash")(
+                            file,
+                            (progress, total) => {
+                                Vars.ui.hudfrag.setHudText(
+                                    "Progress: " +
+                                        progress +
+                                        "/" +
+                                        total +
+                                        " (" +
+                                        Math.floor((100 * progress) / total) +
+                                        "%)"
+                                );
+                            },
+                            (bytes) => {
+                                Vars.ui.hudfrag.toggleHudText(false);
+                                const message =
+                                    "Loaded " + bytes + " bytes from " + file.name() + ".";
+                                Log.info(message);
+                                Vars.ui.hudfrag.showToast("Done! " + message);
+                            },
+                            (e) => {
+                                Log.err(e);
+                                Vars.ui.hudfrag.toggleHudText(false);
+                                Vars.ui.hudfrag.showToast(
+                                    Icon.warning,
+                                    "Failed to flash processor RAM: " + e
+                                );
+                            }
+                        );
                     });
                 })
                 .tooltip("Flash mlogv32 bin file")
+                .size(40);
+
+            buttons
+                .button(Icon.upload, Styles.cleari, () => {
+                    Vars.platform.showFileChooser(false, "bin", (file) => {
+                        memory("dump")(
+                            file,
+                            (progress, total) => {
+                                Vars.ui.hudfrag.setHudText(
+                                    "Progress: " +
+                                        progress +
+                                        "/" +
+                                        total +
+                                        " (" +
+                                        Math.floor((100 * progress) / total) +
+                                        "%)"
+                                );
+                            },
+                            (bytes) => {
+                                Vars.ui.hudfrag.toggleHudText(false);
+                                const message =
+                                    "Dumped " + bytes + " bytes to " + file.name() + ".";
+                                Log.info(message);
+                                Vars.ui.hudfrag.showToast("Done! " + message);
+                            },
+                            (e) => {
+                                Log.err(e);
+                                Vars.ui.hudfrag.toggleHudText(false);
+                                Vars.ui.hudfrag.showToast(
+                                    Icon.warning,
+                                    "Failed to dump processor RAM: " + e
+                                );
+                            }
+                        );
+                    });
+                })
+                .tooltip("Dump mlogv32 memory")
                 .size(40);
         }
     },
 });
 
 /**
- * @param {LogicBuild} processor
- * @returns {boolean}
+ *
+ * @param {number} memoryX
+ * @param {number} memoryY
+ * @param {number} memoryWidth
+ * @param {number} memoryLengthBytes
+ * @param {number} memoryProcLengthVars
+ * @returns {Mlogv32Memory}
  */
-function isMlogv32Processor(processor) {
-    return (
-        processor.executor.optionalVar("MEMORY_X") != null &&
-        processor.executor.optionalVar("MEMORY_Y") != null &&
-        processor.executor.optionalVar("MEMORY_WIDTH") != null &&
-        processor.executor.optionalVar("MEMORY_END") != null &&
-        processor.executor.optionalVar("MEMORY_PROC_SIZE") != null
-    );
-}
+const Mlogv32Memory_new = (
+    memoryX,
+    memoryY,
+    memoryWidth,
+    memoryLengthBytes,
+    memoryProcLengthVars
+) => {
+    /**
+     * @param {number} address
+     * @returns {LogicBuild}
+     */
+    function getProcessor(address) {
+        if (address >= memoryLengthBytes) {
+            throw new Error("Tried to read invalid address " + address + ": out of range");
+        }
+
+        const index = Math.floor(address / 4 / memoryProcLengthVars);
+        const x = memoryX + (index % memoryWidth);
+        const y = memoryY + Math.floor(index / memoryWidth);
+
+        /** @type {LogicBuild | null} */
+        const proc = Vars.world.build(x, y);
+
+        if (proc == null || proc.block.name != "micro-processor") {
+            throw new Error(
+                "Tried to read invalid address " +
+                    address +
+                    ": micro-processor not found at " +
+                    x +
+                    "," +
+                    y
+            );
+        }
+
+        if (
+            proc.executor.vars.length != memoryProcLengthVars + 1 ||
+            proc.executor.vars[1].name != "!!"
+        ) {
+            throw new Error(
+                "Tried to read invalid address " + address + ": incorrect variable layout"
+            );
+        }
+
+        return proc;
+    }
+
+    /**
+     * Calls callback on every word in memory. Return true to keep iterating or false to stop.
+     * @param {(word: LVar, address: number) => boolean} callback
+     * @param {(bytes: number) => any} onProgress
+     * @param {(bytes: number) => any} onFinish
+     * @param {(e: Error) => any} onError
+     * @param {number} startAddress
+     */
+    function forEach(callback, onProgress, onFinish, onError, startAddress) {
+        let address = startAddress;
+        try {
+            for (let i = 0; i < 128; i++) {
+                let proc = getProcessor(address);
+                for (let lvarIndex = 1; lvarIndex <= memoryProcLengthVars; lvarIndex++) {
+                    const lvar = proc.executor.vars[lvarIndex];
+                    if (!callback(lvar, address)) {
+                        onFinish(address + 4);
+                        return;
+                    }
+                    address += 4;
+                }
+                onProgress(address);
+            }
+        } catch (e) {
+            onError(e);
+            return;
+        }
+
+        Time.runTask(1, () => forEach(callback, onProgress, onFinish, onError, address));
+    }
+
+    /**
+     * @param {Fi} file
+     * @param {(progress: number, total: number) => any} onProgress
+     * @param {(bytes: number) => any} onFinish
+     * @param {(e: Error) => any} onError
+     */
+    function flash(file, onProgress, onFinish, onError) {
+        /** @type number[] */
+        const data = file.readBytes();
+        if (data.length % 4 != 0) {
+            onError(new Error("Invalid data alignment, length must be a multiple of 4 bytes!"));
+            return;
+        } else if (data.length > memoryLengthBytes) {
+            onError(new Error("Data too large!"));
+            return;
+        }
+
+        forEach(
+            (word, address) => {
+                const value =
+                    ((data[address] & 0xff) << 24) |
+                    ((data[address + 1] & 0xff) << 16) |
+                    ((data[address + 2] & 0xff) << 8) |
+                    (data[address + 3] & 0xff);
+                word.setnum(value >>> 0);
+                return address + 4 < data.length;
+            },
+            (bytes) => onProgress(bytes, data.length),
+            onFinish,
+            onError,
+            0
+        );
+    }
+
+    /**
+     * @param {Fi} file
+     * @param {(progress: number, total: number) => any} onProgress
+     * @param {(bytes: number) => any} onFinish
+     * @param {(e: Error) => any} onError
+     */
+    function dump(file, onProgress, onFinish, onError) {
+        const writes = file.writes();
+        forEach(
+            (word, address) => {
+                writes.b(((word.numval >> 24) & 0xff) >>> 0);
+                writes.b(((word.numval >> 16) & 0xff) >>> 0);
+                writes.b(((word.numval >> 8) & 0xff) >>> 0);
+                writes.b((word.numval & 0xff) >>> 0);
+                return address + 4 < memoryLengthBytes;
+            },
+            (bytes) => onProgress(bytes, memoryLengthBytes),
+            (bytes) => {
+                writes.close();
+                onFinish(bytes);
+            },
+            onError,
+            0
+        );
+    }
+
+    return (method) => {
+        if (method == "getProcessor") return getProcessor;
+        if (method == "forEach") return forEach;
+        if (method == "flash") return flash;
+        if (method == "dump") return dump;
+        throw new Error("Unsupported method: " + method);
+    };
+};
 
 /**
- * @param {LogicBuild} processor
- * @param {Fi} file
+ * @param {LogicBuild} build
+ * @returns {Mlogv32Memory | null}
  */
-function flashMlogv32Processor(processor, file) {
-    const RAM_X = processor.executor.optionalVar("MEMORY_X").numval;
-    const RAM_Y = processor.executor.optionalVar("MEMORY_Y").numval;
-    const RAM_WIDTH = processor.executor.optionalVar("MEMORY_WIDTH").numval;
-    const RAM_SIZE = processor.executor.optionalVar("MEMORY_END").numval;
-    const RAM_PROC_SIZE = processor.executor.optionalVar("MEMORY_PROC_SIZE").numval;
-
-    /** @type number[] */
-    const data = file.readBytes();
-    if (data.length % 4 != 0) {
-        throw new Error("Invalid data alignment, length must be a multiple of 4 bytes!");
-    } else if (data.length > RAM_SIZE) {
-        throw new Error("Data too large!");
+function Mlogv32Memory_of(build) {
+    const x = build.executor.optionalVar("MEMORY_X");
+    const y = build.executor.optionalVar("MEMORY_Y");
+    const width = build.executor.optionalVar("MEMORY_WIDTH");
+    const lengthBytes = build.executor.optionalVar("MEMORY_END");
+    const procLengthVars = build.executor.optionalVar("MEMORY_PROC_SIZE");
+    if (x == null || y == null || width == null || lengthBytes == null || procLengthVars == null) {
+        return null;
     }
-
-    let ramIndex = 0;
-    let varIndex = 1; // 0 is @counter
-
-    /** @returns {LogicBuild} */
-    function getRam() {
-        const x = RAM_X + (ramIndex % RAM_WIDTH);
-        const y = RAM_Y + Math.floor(ramIndex / RAM_WIDTH);
-
-        const ram = Vars.world.build(x, y);
-        if (ram == null || ram.executor.vars[1].name != "!!") {
-            throw new Error("Tried to write to invalid RAM proc at " + x + "," + y);
-        }
-
-        return ram;
-    }
-
-    let ram = getRam();
-
-    for (let i = 0; i < data.length; i += 4) {
-        if (varIndex > RAM_PROC_SIZE) {
-            ramIndex++;
-            varIndex = 1;
-            ram = getRam();
-        }
-
-        const value =
-            ((data[i] & 0xff) << 24) |
-            ((data[i + 1] & 0xff) << 16) |
-            ((data[i + 2] & 0xff) << 8) |
-            (data[i + 3] & 0xff);
-
-        // i hate javascript
-        ram.executor.vars[varIndex].setnum(value >>> 0);
-        varIndex++;
-    }
-
-    Vars.ui.hudfrag.showToast(
-        "Done! Loaded " +
-            data.length +
-            " bytes from " +
-            file.name() +
-            " into " +
-            (ramIndex + 1) +
-            " RAM procs."
+    return Mlogv32Memory_new(
+        x.numval,
+        y.numval,
+        width.numval,
+        lengthBytes.numval,
+        procLengthVars.numval
     );
 }

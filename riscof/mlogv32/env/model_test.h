c59a1d1cfc9cba9ef1c8dbb28554584afe46324c
object-Object <object@objectobject.ca>
2025-06-14 20:22:37 -0400

    Implement RVMODEL_IO_WRITE_STR and RVMODEL_IO_ASSERT_GPR_EQ

--- riscof/mlogv32/env/model_test.h
+++ riscof/mlogv32/env/model_test.h
@@ -11,7 +11,7 @@
   li t0, 0xfffffff0; \
   sw zero, 0(t0);
 
-// initialize icache, .text, .data, and .bss
+// initialize icache, uart0, .text, .data, and .bss
 // see https://github.com/riscv-non-isa/riscv-arch-test/issues/202
 #define RVMODEL_BOOT \
   .option norelax; \
@@ -19,6 +19,10 @@
   \
   la t0, __etext_rom; \
   .insn i CUSTOM_0, 0, zero, t0, 0; \
+  \
+  li t0, 0xf0000010; \
+  li t1, 0b111; \
+  sb t1, 0x08(t0); \
   \
   la t0, __sitext; \
   la t1, __stext; \
@@ -70,11 +74,78 @@ mlogv32_bss_done: \
 //RVTEST_IO_INIT
 #define RVMODEL_IO_INIT
 //RVTEST_IO_WRITE_STR
-#define RVMODEL_IO_WRITE_STR(_R, _STR)
+#define RVMODEL_IO_WRITE_STR(ScrReg, ...) \
+  la ScrReg, 2f; \
+  sw a0, 0(ScrReg); \
+  sw a1, 4(ScrReg); \
+  sw a2, 8(ScrReg); \
+  la a2, 3f; \
+0: \
+  lbu a0, 0(a2); \
+  beqz a0, 1f; \
+  li a1, 0xf0000010; \
+  sb a0, 0(a1); \
+  addi a2, a2, 1; \
+  j 0b; \
+1: \
+  la ScrReg, 2f; \
+  lw a0, 0(ScrReg); \
+  lw a1, 4(ScrReg); \
+  lw a2, 8(ScrReg); \
+  .pushsection .data.string,"aw",@progbits; \
+  .balign 4; \
+2: .word 0, 0, 0; \
+3: .ascii __VA_ARGS__; .byte 0; \
+  .popsection;
+
+#define MLOGV32_IO_WRITE_HEX__CHAR(Shift) \
+  srli a0, a2, Shift; \
+  andi a0, a0, 0xf; \
+  addi a0, a0, 48; \
+  li a1, 58; \
+  bltu a0, a1, 0f; \
+  addi a0, a0, 7; \
+0: \
+  li a1, 0xf0000010; \
+  sb a0, 0(a1);
+
+#define MLOGV32_IO_WRITE_HEX(ScrReg, Reg) \
+  la ScrReg, 1f; \
+  sw Reg, 0(ScrReg); \
+  sw a0, 4(ScrReg); \
+  sw a1, 8(ScrReg); \
+  sw a2, 12(ScrReg); \
+  lw a2, 0(ScrReg); \
+  \
+  MLOGV32_IO_WRITE_HEX__CHAR(28) \
+  MLOGV32_IO_WRITE_HEX__CHAR(24) \
+  MLOGV32_IO_WRITE_HEX__CHAR(20) \
+  MLOGV32_IO_WRITE_HEX__CHAR(16) \
+  MLOGV32_IO_WRITE_HEX__CHAR(12) \
+  MLOGV32_IO_WRITE_HEX__CHAR(8) \
+  MLOGV32_IO_WRITE_HEX__CHAR(4) \
+  MLOGV32_IO_WRITE_HEX__CHAR(0) \
+  \
+  la ScrReg, 2f; \
+  lw Reg, 0(ScrReg); \
+  lw a0, 4(ScrReg); \
+  lw a1, 8(ScrReg); \
+  lw a2, 12(ScrReg); \
+  .pushsection .data.string,"aw",@progbits; \
+  .balign 4; \
+1: .word 0, 0, 0, 0; \
+  .popsection;
+
 //RVTEST_IO_CHECK
 #define RVMODEL_IO_CHECK()
 //RVTEST_IO_ASSERT_GPR_EQ
-#define RVMODEL_IO_ASSERT_GPR_EQ(_S, _R, _I)
+#define RVMODEL_IO_ASSERT_GPR_EQ(ScrReg, Reg, Value) \
+  li ScrReg, Value; \
+  beq ScrReg, Reg, 4f; \
+  RVMODEL_IO_WRITE_STR(ScrReg, "Expected ", #Reg, " == ", #Value, ", got 0x") \
+  MLOGV32_IO_WRITE_HEX(ScrReg, Reg) \
+  RVMODEL_IO_WRITE_STR(ScrReg, "\n") \
+4:
 //RVTEST_IO_ASSERT_SFPR_EQ
 #define RVMODEL_IO_ASSERT_SFPR_EQ(_F, _R, _I)
 //RVTEST_IO_ASSERT_DFPR_EQ

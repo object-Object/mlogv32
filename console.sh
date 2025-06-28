2b6820f275927dc2ae1307e742352fa8e9acdd2a
object-Object <object@objectobject.ca>
2025-06-28 12:15:21 -0400

    Move micropython/repl.sh to console.sh

--- console.sh
+++ console.sh
@@ -8,7 +8,7 @@ else
 fi
 
 {
-    echo "{\"type\": \"serial\", \"device\": \"uart0\", \"overrun\": false, \"direction\": \"${DIRECTION}\", \"disconnectOnHalt\": true}"
+    echo "{\"type\": \"serial\", \"device\": \"${DEVICE:=uart0}\", \"overrun\": false, \"direction\": \"${DIRECTION}\", \"disconnectOnHalt\": true}"
     cat
 } | netcat -v "${HOST:=localhost}" "${PORT:=5000}"
 

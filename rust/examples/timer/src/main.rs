94b3b1315d1f88beb2ff682fee6d6466f5aede6c
object-Object <object@objectobject.ca>
2025-06-28 15:32:23 -0400

    Update timer example

--- rust/examples/timer/src/main.rs
+++ rust/examples/timer/src/main.rs
@@ -5,7 +5,7 @@ use core::{cell::RefCell, hint};
 
 use critical_section::Mutex;
 use itoa::Buffer;
-use mlogv32::io::print_str;
+use mlogv32::io::UartPort;
 use riscv::{
     interrupt::{self, Interrupt},
     register::mie,
@@ -15,9 +15,14 @@ use riscv_rt::core_interrupt;
 static TIMER: Mutex<RefCell<MmioMachineTimer<'static>>> =
     unsafe { Mutex::new(RefCell::new(MachineTimer::new_mmio_at(0xf0000000))) };
 
+static UART0: Mutex<RefCell<Option<UartPort>>> = Mutex::new(RefCell::new(None));
+
 #[mlogv32::entry]
 fn main() -> ! {
     critical_section::with(|cs| {
+        let mut uart0 = UART0.borrow_ref_mut(cs);
+        uart0.replace(unsafe { UartPort::new_uart0(253) });
+
         let mut timer = TIMER.borrow_ref_mut(cs);
         timer.write_mtime(0);
         timer.write_mtimecmp(1000);
@@ -40,8 +45,11 @@ fn machine_timer() {
         let mtime = timer.read_mtime();
         timer.write_mtimecmp(mtime + 1000);
 
+        let mut uart0 = UART0.borrow_ref_mut(cs);
+        let uart0 = uart0.as_mut().unwrap();
         let mut buf = Buffer::new();
-        print_str(buf.format(mtime));
+        uart0.blocking_write(buf.format(mtime).as_bytes());
+        uart0.blocking_write(b"\n");
     });
 }
 
